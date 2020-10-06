5195229facb307db35eb3a9ce6ae6f9a80eb35e9
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-10-06 13:15:04 -0700

    saved new

--- my_file/iterations.js
+++ my_file/iterations.js
@@ -4,5 +4,4 @@ const arr = ["a", "b","c"]
 // Looping through array using a for loop
 for (let i = 0; i < arr.length; i += 1) {
     console.log("index", i, "value", arr[i]);
-    console.log("hello world")
   }
